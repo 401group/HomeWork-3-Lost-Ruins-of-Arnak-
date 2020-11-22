b81fd09508b1edd4384e26c0248e4c04b396bb59
Adinma <56094701+Adinma@users.noreply.github.com>
2020-11-21 21:08:32 -0800

    Update DeckTest.java

--- src/test/java/game/DeckTest.java
+++ src/test/java/game/DeckTest.java
@@ -1,14 +1,22 @@
 package game;
 
-public class DeckTest {
+public class DeckTest{
 
 	public DeckTest() {
-		// TODO Auto-generated constructor stub
 	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	// This method tests  returns the size of a Deck
+	@Test
+	public void returnSizeOfHand(){
+		Card one = new Card("Fear", 1, 2);//New Card
+		Card two = new Card("Gold", 5, 7);//New Card
+		Deck d1 = new Deck();
+		
+		//New cards added to Deck
+		d1.addCard(one); 
+		d1.addCard(two);
+		
+		//Test to see if size is equal to 2
+		assertEquals(2, h1.getSize());
 	}
-
 }
