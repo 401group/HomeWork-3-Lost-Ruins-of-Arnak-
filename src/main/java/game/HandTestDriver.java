afe9e973c1caff259af565fe924081ceb963830a
Adinma <56094701+Adinma@users.noreply.github.com>
2020-11-21 18:32:02 -0800

    Update HandTestDriver.java

--- src/main/java/game/HandTestDriver.java
+++ src/main/java/game/HandTestDriver.java
@@ -6,11 +6,18 @@ import org.junit.Assert.asserEquals;
 public class HandTestDriver {
 
 	public HandTestDriver() {
-		// TODO Auto-generated constructor stub
 	}
-
-	@Test{
-		Hand p1 = new Hand();
-		p1.addToHand(Card newCard)
+	
+	// This method tests  returns the size of a players hand
+	@Test
+	public void returnSizeOfHand(){
+		Card one = new Card("FearCard", 1, 2); //New card
+		Hand h1 = new Hand();
+		
+		//New card added to players hand
+		h1.addToHand(one); 
+		
+		//Test to see if size is equal to 1
+		assertEquals(expected: 1, h1.getSize());
 	}
 }
